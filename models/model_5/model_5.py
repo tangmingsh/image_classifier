b152712c9697b9aabc16211b29ba292c961303d6
tangmingsh <68671389+tangmingsh@users.noreply.github.com>
2020-08-30 13:32:12 +0800

    fix bug ValueError: Negative   for Keras 2
    ValueError: Negative dimension size caused by subtracting 2 from 1 for 'max_pooling2d

--- models/model_5/model_5.py
+++ models/model_5/model_5.py
@@ -1,19 +1,51 @@
+# model = Sequential()
+
+# model.add(Conv2D(32, 3, 3, border_mode='same', input_shape=input_shape, activation='relu'))
+# model.add(Conv2D(32, 3, 3, border_mode='same', activation='relu'))
+# model.add(MaxPooling2D(pool_size=(2, 2)))
+
+# model.add(Conv2D(64, 3, 3, border_mode='same', activation='relu'))
+# model.add(Conv2D(64, 3, 3, border_mode='same', activation='relu'))
+# model.add(MaxPooling2D(pool_size=(2, 2)))
+
+# model.add(Conv2D(128, 3, 3, border_mode='same', activation='relu'))
+# model.add(Conv2D(128, 3, 3, border_mode='same', activation='relu'))
+# model.add(MaxPooling2D(pool_size=(2, 2)))
+
+# model.add(Conv2D(256, 3, 3, border_mode='same', activation='relu'))
+# model.add(Conv2D(256, 3, 3, border_mode='same', activation='relu'))
+# model.add(MaxPooling2D(pool_size=(2, 2)))
+
+# model.add(Flatten())
+# model.add(Dense(256, activation='relu'))
+# model.add(Dropout(0.5))
+
+# model.add(Dense(256, activation='relu'))
+# model.add(Dropout(0.5))
+
+# model.add(Dense(1))
+# model.add(Activation('sigmoid'))
+    
+# model.compile(loss='binary_crossentropy',
+#             optimizer=RMSprop(lr=0.0001),
+#             metrics=['accuracy'])
+
 model = Sequential()
 
-model.add(Conv2D(32, 3, 3, border_mode='same', input_shape=input_shape, activation='relu'))
-model.add(Conv2D(32, 3, 3, border_mode='same', activation='relu'))
+model.add(Conv2D(32, (3, 3), padding='same', input_shape=(150,150,3), activation='relu'))
+model.add(Conv2D(32, (3, 3), padding='same', activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Conv2D(64, 3, 3, border_mode='same', activation='relu'))
-model.add(Conv2D(64, 3, 3, border_mode='same', activation='relu'))
+model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
+model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Conv2D(128, 3, 3, border_mode='same', activation='relu'))
-model.add(Conv2D(128, 3, 3, border_mode='same', activation='relu'))
+model.add(Conv2D(128, (3, 3), padding='same', activation='relu'))
+model.add(Conv2D(128, (3, 3), padding='same', activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Conv2D(256, 3, 3, border_mode='same', activation='relu'))
-model.add(Conv2D(256, 3, 3, border_mode='same', activation='relu'))
+model.add(Conv2D(256, (3, 3), padding='same', activation='relu'))
+model.add(Conv2D(256, (3, 3), padding='same', activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Flatten())
@@ -23,9 +55,9 @@ model.add(Dropout(0.5))
 model.add(Dense(256, activation='relu'))
 model.add(Dropout(0.5))
 
-model.add(Dense(1))
-model.add(Activation('sigmoid'))
-    
+model.add(Dense(1,activation='sigmoid'))
+# model.add(activation='sigmoid')
+opt = tf.keras.optimizers.RMSprop(lr=0.0001)
 model.compile(loss='binary_crossentropy',
-            optimizer=RMSprop(lr=0.0001),
+            optimizer= opt,
             metrics=['accuracy'])
